ac8c7b3d0b27081aa1793539892ba8d90b543c5c
pindika <niwiace@gmail.com>
2018-08-30 16:59:47 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <html>
   <header>
-    <title> A Fundraising & Crowdfunding  Sytem in Rwanda
+    <title> A Fundraising & Crowdfunding  Sytem in Rwanda </title>
   </header>
     <body>
       <p>Body can come in this place</p>
